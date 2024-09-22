63290565b1c930884dbedbafe15c95f1d06fcfe0
Griefer <Griefer@HOME-PC>
2024-09-22 16:02:48 +0300

    Добавьте файлы проекта.

---
+++ Config.cs
@@ -0,0 +1,8 @@
+﻿using Exiled.API.Interfaces;
+using System;
+
+public class Config : IConfig
+{
+    public bool IsEnabled { get; set; } = true;
+    public bool Debug { get; set; } = false;
+}
---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetPrefix
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+    }
+}
+
